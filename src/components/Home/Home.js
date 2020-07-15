e5da46c813170eca5ea0923d5850cd60807017c9
UsamaSohail94 <37038343+UsamaSohail94@users.noreply.github.com>
2020-07-15 13:06:31 +0500

    Shoe component edited

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,8 @@ import womens from "../../images/womens.jpg"
 import kids from "../../images/kids.jpg"
 import {Grid, makeStyles, Typography, Paper} from "@material-ui/core"
 
+import {Link } from 'react-router-dom'
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -18,6 +20,9 @@ const useStyles = makeStyles((theme) => ({
   },
   collectionContainer:{
     padding:10,
+  },
+  link:{
+    textDecoration:"none"
   }
 }));
 
@@ -47,15 +52,18 @@ const Home = () => {
               className={classes.collectionContainer}
         >
           <Grid item xs={12} sm={6} md={4} width="100%">
+          <Link to='newArrival' className={classes.link}> 
             <Paper width="100%">
               <img src={mens} alt="mens" width="100%" />
               <Typography variant="h5" align="center" className={classes.whatIsNew}>
                 Men's New Arrival
               </Typography>
             </Paper>
+          </Link>
           </Grid>
 
           <Grid item xs={12} sm={6} md={4} width="100%">
+            <Link to='newArrival'  className={classes.link}>
           <Paper width="100%">
             
             <img src={womens} alt="womens" width="100%" />  
@@ -63,15 +71,18 @@ const Home = () => {
               Women's New Arrival
             </Typography>
             </Paper>
+            </Link>
           </Grid>
 
           <Grid item xs={12} sm={6} md={4} width="100%">
+          <Link to="newArrival" className={classes.link}>
             <Paper width="100%">
             <img src={kids} alt="kids" width="100%" />
               <Typography variant="h5" align="center" className={classes.whatIsNew}>
                 Kid's New Arrival
               </Typography>
               </Paper>
+          </Link>
           </Grid>
         </Grid>
       </Grid>
--- src/components/Shoe/Shoe.js
+++ src/components/Shoe/Shoe.js
@@ -1,19 +1,62 @@
 import React,{useContext} from 'react'
 import {productContext} from '../../App'
 import { useParams } from 'react-router-dom'
+import { Grid, makeStyles, Typography, Paper } from '@material-ui/core'
+
+const useStyles = makeStyles((theme)=>({
+  root:{
+    flexGrow:1,
+    padding:20
+  },
+  container:{
+    Height:100,
+    width:"100%",
+    
+    paddin:20,
+
+    
+    backgroundColor:"lightYellow"
+  }
+
+}))
 
 
 
 const Shoe = () => {
+
+const classes = useStyles()
+
 const shoes = useContext(productContext)
 const { shoeID } = useParams()
 const shoe = shoes[shoeID]
-const {name, img} = shoe
+const {name, img, price} = shoe
     return (
-    <div>
-    <h3>{name}</h3>
-    <img src={img} alt={name}/>
-      
+    <div className={classes.root}>
+    
+    <Grid 
+      container
+      justify= 'center'
+      className={classes.container}
+      spacing={4}
+      >
+      <Grid item xs={8}  width="100%" height="100%"  style={{backgroundColor:""}}>
+        <Paper width="100%" height={250} >
+          <img src={img} alt={name} width="100%"  />
+        </Paper>
+        
+      </Grid>
+
+      <Grid  xs={4}  width="100%" style={{backgroundColor:""}}>
+        <Paper  width="75%" height="400px" style={{marginTop:20}}>
+          <Typography variant="h3" align='center'>
+            {name}
+          </Typography>
+          <Typography variant="h4" component="h2" align='center'>
+            Price: ${price}
+          </Typography>
+        </Paper>
+      </Grid>
+    </Grid>  
     </div>
   )
 }
